5677f1e3b4c926ffaa2478ca557e639db038e650
Gilberto Encarnacion <gilbert@cphandheld.com>
2014-12-03 16:41:57 -0600

    added ramdisk

---
+++ config.h
@@ -0,0 +1,17 @@
+/*
+ * config.h
+ *
+ *  Created on: Dec 2, 2014
+ *      Author: titan
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+typedef struct AppConfig_t
+{
+	char wan_device_address;
+
+} AppConfig_t;
+
+#endif /* CONFIG_H_ */
